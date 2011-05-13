9d32225afbf282536301b88c2c5758ee1e79eee8
Amrita Gosh <Amrita@Amrita-Ghoshs-Mac-mini.local>
2011-05-13 11:50:39 +0530

    Initial Commit

---
+++ AdmobiPhone/AdmobiPhoneAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AdmobiPhoneAppDelegate.h
+//  AdmobiPhone
+//
+//  Created by Amrita Gosh on 13/05/11.
+//  Copyright 2011 Chakra Interactive Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AdmobiPhoneViewController;
+
+@interface AdmobiPhoneAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet AdmobiPhoneViewController *viewController;
+
+@end
---
+++ AdmobiPhone/AdmobiPhoneViewController.h
@@ -0,0 +1,15 @@
+//
+//  AdmobiPhoneViewController.h
+//  AdmobiPhone
+//
+//  Created by Amrita Gosh on 13/05/11.
+//  Copyright 2011 Chakra Interactive Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdmobiPhoneViewController : UIViewController {
+    
+}
+
+@end
